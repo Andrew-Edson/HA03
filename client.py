284d4853007c1a0d92dd7bb6d6876dbac916fdd3
andrewEdson <aedson8175@hotmail.com>
2024-10-17 13:05:16 -0700

    File sharing enabled for both client and server

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import os
 
 
+# Starts client and connects to server
 def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("127.0.0.1", 12345))
@@ -21,9 +22,12 @@ def start_client():
         else:
             client.send(message.encode("utf-8"))
             response = client.recv(1024).decode("utf-8")
+            if response == "FILE":
+                recieve_file(client)
             print(f"Server: {response}")
 
 
+# Sends file to server
 def send_file(client):
     file_path = input("Enter file path: ")
 
@@ -31,13 +35,9 @@ def send_file(client):
 
         client.send("FILE".encode("utf-8"))
         file_name = os.path.basename(file_path)
-        client.send("FILE".encode("utf-8"))
+        client.send(file_name.encode("utf-8"))
         file_size = os.path.getsize(file_path)
         client.send(str(file_size).encode("utf-8"))
-
-        # client.send("FILE".encode("utf-8"))
-        # client.send(file_name.encode("utf-8"))
-        # client.send(str(file_size).encode("utf-8"))
 
         with open(file_path, "rb") as file:
             while file_size > 0:
@@ -50,4 +50,19 @@ def send_file(client):
         print("File not found")
 
 
+# Receives file from server
+def recieve_file(client):
+    file_name = client.recv(1024).decode("utf-8")
+    file_size = int(client.recv(1024).decode("utf-8"))
+
+    with open(f"client_files\{file_name}", "wb") as file:
+        while file_size > 0:
+            data = client.recv(1024)
+            file.write(data)
+            file_size -= len(data)
+
+    print(f"File {file_name} received")
+
+
+# Starts client
 start_client()
--- server.py
+++ server.py
@@ -3,12 +3,14 @@ import threading
 import os
 
 
+# Starts server and binds to socket 12345
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("127.0.0.1", 12345))
     server.listen(5)
     print("Server started")
 
+    # Accepts client connections and starts a new thread for each client
     while True:
         client, addr = server.accept()
         print(f"Connection from {addr}")
@@ -16,6 +18,7 @@ def start_server():
         client_thread.start()
 
 
+# Handles client messages and file transfers
 def handle_client(client):
     while True:
         message = client.recv(1024).decode("utf-8")
@@ -27,15 +30,19 @@ def handle_client(client):
         else:
             print(f"Client: {message}")
 
-            response = input("Server: ")
-            client.send(response.encode("utf-8"))
+            response = input("Server Response (Input 'FILE' to send a file)\n")
+            if response == "FILE":
+                send_file(client)
+            else:
+                client.send(response.encode("utf-8"))
 
 
+# Receives file from client
 def receive_file(client):
     file_name = client.recv(1024).decode("utf-8")
     file_size = int(client.recv(1024).decode("utf-8"))
 
-    with open(file_name, "wb") as file:
+    with open(f"server_files\{file_name}", "wb") as file:
         while file_size > 0:
             data = client.recv(1024)
             file.write(data)
@@ -44,4 +51,28 @@ def receive_file(client):
     print(f"File {file_name} received")
 
 
+# Sends file to client
+def send_file(client):
+    file_path = input("Enter file path: ")
+
+    if os.path.exists(file_path):
+
+        client.send("FILE".encode("utf-8"))
+        file_name = os.path.basename(file_path)
+        client.send(file_name.encode("utf-8"))
+        file_size = os.path.getsize(file_path)
+        client.send(str(file_size).encode("utf-8"))
+
+        with open(file_path, "rb") as file:
+            while file_size > 0:
+                data = file.read(1024)
+                client.send(data)
+                file_size -= len(data)
+
+        print(f"File {file_name} sent")
+    else:
+        print("File not found")
+
+
+# Starts server
 start_server()
